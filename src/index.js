319d34ea78d983bb0ba543325c5966a78cdef1b2
Oleksandr Khrapak <al.khrapak@gmail.com>
2021-10-18 02:28:05 +0300

    add body class

--- src/index.js
+++ src/index.js
@@ -18,3 +18,26 @@ foodContainerRef.insertAdjacentHTML('beforeend', cardsFood);
 function createCardsFood(food) {
   return cardFoodTpl(food);
 }
+themeStart();
+
+function themeStart() {
+  if (!localStorage.length) {
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+  bodyRef.classList.add(localStorage.getItem('theme'));
+}
+
+themeSwitchToggleRef.addEventListener('change', switchTheme);
+
+function switchTheme(evt) {
+  bodyRef.classList.toggle(Theme.DARK);
+  if (bodyRef.classList.contains(Theme.DARK)) {
+    localStorage.setItem('theme', Theme.DARK);
+    // themeSwitchToggleRef.classList.add('theme-switch__toggle:checked');
+  } else {
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+  // console.log(evt.target.value);
+  //   bodyRef.classList.remove(Theme.LIGHT);
+  //   bodyRef.classList.add(Theme.DARK);
+}
